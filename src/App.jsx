98e2300a699331f6b4fe033ea2c79623a1086230
FlorGazzo <gazzoflorencia.pa@gmail.com>
2023-05-11 20:52:41 -0300

    commit de mi primer entrega

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ItemListContainer from './Components/ItemListContainer'
+import NavBar from './Components/NavBar'
+
+const App = () => {
+  return (
+    <>
+    <NavBar/>
+    <ItemListContainer greeting={"Bienvenidos a Classique"}/>
+    </>
+  )
+}
+
+export default App;
+
+
